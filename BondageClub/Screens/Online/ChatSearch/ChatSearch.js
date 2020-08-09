6617a9450421d694ca6e0a20d3bb1c0d2ee26f23
Ace <>
2020-08-09 16:06:12 -0400

    New permission mode

--- BondageClub/Screens/Online/ChatSearch/ChatSearch.js
+++ BondageClub/Screens/Online/ChatSearch/ChatSearch.js
@@ -7,13 +7,14 @@ var ChatSearchLastQuerySearchHiddenRooms = 0;
 var ChatSearchLastQueryJoin = "";
 var ChatSearchLastQueryJoinTime = 0;
 var ChatSearchResultOffset = 0;
-var ChatSearchRoomsPerPage = 23;
+var ChatSearchRoomsPerPage = 24;
 var ChatSearchMessage = "";
 var ChatSearchLeaveRoom = "MainHall";
 var ChatSearchSafewordAppearance = null;
 var ChatSearchSafewordPose = null;
 var ChatSearchPreviousActivePose = null;
 var ChatSearchIgnoredRooms = [];
+var ChatSearchMode = "";
 
 /**
  * Loads the chat search screen properties, creates the inputs and loads up the first 24 rooms.
@@ -36,9 +37,72 @@ function ChatSearchLoad() {
  */
 function ChatSearchRun() {
 
-	// If we can show the chat room search result
+	if (ChatSearchMode == "")
+		ChatSearchNormalDraw();
+	else if (ChatSearchMode == "Filter")
+		ChatSearchPermissionDraw();
+	
+	// Draw the bottom controls
+	if (ChatSearchMessage == "") ChatSearchMessage = "EnterName";
+	DrawText(TextGet(ChatSearchMessage), 255, 935, "White", "Gray");
+	ElementPosition("InputSearch",  740, 926, 470);
+	DrawButton(980, 898, 280, 64, TextGet("SearchRoom"), "White");
+	DrawButton(1280, 898, 280, 64, TextGet("CreateRoom"), "White");
+	DrawButton(1685, 885, 90, 90, "", "White", ChatSearchMode != "Filter" ? "Icons/DialogPermissionMode.png" : "Icons/DialogNormalMode.png", TextGet(ChatSearchMode != "Filter" ?  "FilterMode" : "NormalMode"));
+	DrawButton(1785, 885, 90, 90, "", "White", "Icons/FriendList.png");
+	DrawButton(1885, 885, 90, 90, "", "White", "Icons/Exit.png");
+	if (ChatSearchResult.length + (ChatSearchMode != "Filter" ? 0 : ChatSearchIgnoredRooms.length) > ChatSearchRoomsPerPage) DrawButton(1585, 885, 90, 90, "", "White", "Icons/Next.png");
+}
+
+/**
+ * Handles the click events on the chat search screen. Is called from CommonClick()
+ * @returns {void} - Nothing
+ */
+function ChatSearchClick() {
+	if ((MouseX >= 25) && (MouseX < 1975) && (MouseY >= 25) && (MouseY < 875)) { 
+		if (ChatSearchMode == "Filter") ChatSearchClickPermission();
+		if (ChatSearchMode == "" && Array.isArray(ChatSearchResult) && (ChatSearchResult.length >= 1)) ChatSearchJoin();
+	}
+	if (MouseIn(980, 898, 280, 64)) ChatSearchQuery();
+	if (MouseIn(1280, 898, 280, 64)) CommonSetScreen("Online", "ChatCreate");
+	if (MouseIn(1585, 885, 90, 90)) { 
+		ChatSearchResultOffset += ChatSearchRoomsPerPage;
+		if (ChatSearchResultOffset >= ChatSearchResult.length + (ChatSearchMode != "Filter" ? 0 : ChatSearchIgnoredRooms.length)) ChatSearchResultOffset = 0;
+	}
+	if (MouseIn(1685, 885, 90, 90)) {
+		ChatSearchMode = (ChatSearchMode == "Filter" ? "" : "Filter");
+		ChatSearchQuery();
+	}
+	if (MouseIn(1785, 885, 90, 90)) { ElementRemove("InputSearch"); CommonSetScreen("Character", "FriendList"); FriendListReturn = "ChatSearch"; }
+	if (MouseIn(1885, 885, 90, 90)) ChatSearchExit();
+}
+
+/**
+ * Handles the key presses while in the chat search screen. When the user presses enter, we lauch the search query.
+ * @returns {void} - Nothing
+ */
+function ChatSearchKeyDown() {
+	if (KeyPress == 13) ChatSearchQuery();
+}
+
+/**
+ * Handles exiting from the chat search screen, removes the input.
+ * @returns {void} - Nothing
+ */
+function ChatSearchExit() {
+	ChatSearchPreviousActivePose = Player.ActivePose;
+	ElementRemove("InputSearch");
+	CommonSetScreen("Room", ChatSearchLeaveRoom);
+}
+
+/**
+ * Draws the list of rooms in normal mode.
+ * @returns {void} - Nothing
+ */
+function ChatSearchNormalDraw() { 
+	// If we can show the chat room search result in normal mode
 	if (Array.isArray(ChatSearchResult) && (ChatSearchResult.length >= 1)) {
-		
+			
 		// Show up to 24 results
 		var X = 25;
 		var Y = 25;
@@ -48,7 +112,7 @@ function ChatSearchRun() {
 			var HasFriends = ChatSearchResult[C].Friends != null && ChatSearchResult[C].Friends.length > 0;
 			var IsFull = ChatSearchResult[C].MemberCount == ChatSearchResult[C].MemberLimit;
 			DrawButton(X, Y, 630, 85, "", (HasFriends && IsFull ? "#448855" : HasFriends ? "#CFFFCF" : IsFull ? "#666" : "White"));
-			DrawTextFit( (ChatSearchResult[C].Friends != null  && ChatSearchResult[C].Friends.length > 0 ? "(" + ChatSearchResult[C].Friends.length + ") " : "") + ChatSearchResult[C].Name + " - " + ChatSearchResult[C].Creator + " " + ChatSearchResult[C].MemberCount + "/" + ChatSearchResult[C].MemberLimit + "", X + 315, Y + 25, 620, "black");
+			DrawTextFit((ChatSearchResult[C].Friends != null && ChatSearchResult[C].Friends.length > 0 ? "(" + ChatSearchResult[C].Friends.length + ") " : "") + ChatSearchResult[C].Name + " - " + ChatSearchResult[C].Creator + " " + ChatSearchResult[C].MemberCount + "/" + ChatSearchResult[C].MemberLimit + "", X + 315, Y + 25, 620, "black");
 			DrawTextFit(ChatSearchResult[C].Description, X + 315, Y + 62, 620, "black");
 
 			// Moves the next window position
@@ -85,75 +149,68 @@ function ChatSearchRun() {
 		}
 
 	} else DrawText(TextGet("NoChatRoomFound"), 1000, 450, "White", "Gray");
-
-	// Draw the bottom controls
-	if (ChatSearchMessage == "") ChatSearchMessage = "EnterName";
-	DrawText(TextGet(ChatSearchMessage), 255, 935, "White", "Gray");
-	ElementPosition("InputSearch",  740, 926, 470);
-	DrawButton(980, 898, 280, 64, TextGet("SearchRoom"), "White");
-	DrawButton(1280, 898, 280, 64, TextGet("HideRoom"), "White");
-	DrawButton(1580, 898, 280, 64, TextGet("CreateRoom"), "White");
-	if (ChatSearchIgnoredRooms.length > 0) DrawButton(1785, 785, 90, 90, "", "White", "Icons/Cancel.png", TextGet("ClearFilter"));
-	if (ChatSearchResult.length > ChatSearchRoomsPerPage) DrawButton(1685, 785, 90, 90, "", "White", "Icons/Next.png");
-	DrawButton(1885, 785, 90, 90, "", "White", "Icons/FriendList.png");
-	DrawButton(1885, 885, 90, 90, "", "White", "Icons/Exit.png");
 }
 
 /**
- * Handles the click events on the chat search screen. Is called from CommonClick()
+ * Draws the list of rooms in permission mode.
  * @returns {void} - Nothing
  */
-function ChatSearchClick() {
-	if ((MouseX >= 25) && (MouseX < 1975) && (MouseY >= 25) && (MouseY < 875) && Array.isArray(ChatSearchResult) && (ChatSearchResult.length >= 1)) ChatSearchJoin();
-	if (MouseIn(980, 898, 280, 64)) ChatSearchQuery();
-	if (MouseIn(1280, 898, 280, 64)) { 
-		var RoomName = ElementValue("InputSearch").toUpperCase().trim();
-		var LN = /^[a-zA-Z0-9 ]+$/; 
-		if (RoomName != "" && ChatSearchIgnoredRooms.indexOf(RoomName) == -1 && RoomName.match(LN)) {
-			ChatSearchIgnoredRooms.push(RoomName);
-			ChatSearchMessage = "RoomHidden";
-			ElementValue("InputSearch", "");
-			ChatSearchQuery();
+function ChatSearchPermissionDraw() { 
+	if (Array.isArray(ChatSearchResult) && (ChatSearchResult.length + ChatSearchIgnoredRooms.length >= 1)) {
+			
+		// Show results + previously hidden rooms
+		var X = 25;
+		var Y = 25;
+		var ShownRooms = 0;
+		
+		for (let C = ChatSearchResultOffset; C < ChatSearchResult.length && ShownRooms < ChatSearchRoomsPerPage; C++) {
+			var isIgnored = ChatSearchIgnoredRooms.includes(ChatSearchResult[C].Name.toUpperCase());
+			var Hover = (MouseX >= X) && (MouseX <= X + 630) && (MouseY >= Y) && (MouseY <= Y + 85) && !CommonIsMobile;
+			// Draw the room rectangle
+			DrawRect(X, Y, 630, 85, isIgnored ? (Hover ? "red" : "pink") : ( Hover ? "green" : "lime"));
+			DrawTextFit((ChatSearchResult[C].Friends != null && ChatSearchResult[C].Friends.length > 0 ? "(" + ChatSearchResult[C].Friends.length + ") " : "") + ChatSearchResult[C].Name + " - " + ChatSearchResult[C].Creator + " " + ChatSearchResult[C].MemberCount + "/" + ChatSearchResult[C].MemberLimit + "", X + 315, Y + 25, 620, "black");
+			DrawTextFit(ChatSearchResult[C].Description, X + 315, Y + 62, 620, "black");
+
+			// Moves the next window position
+			X = X + 660;
+			if (X > 1500) {
+				X = 25;
+				Y = Y + 109;
+			}
+			ShownRooms++;
 		}
-	}
-	if (MouseIn(1580, 898, 280, 64)) CommonSetScreen("Online", "ChatCreate");
-	if (MouseIn(1785, 785, 90, 90)) {
-		ChatSearchIgnoredRooms = [];
-		ChatSearchQuery();
-	}
-	if (MouseIn(1685, 785, 90, 90)) { 
-		ChatSearchResultOffset += ChatSearchRoomsPerPage;
-		if (ChatSearchResultOffset >= ChatSearchResult.length) ChatSearchResultOffset = 0;
-	}
-	if (MouseIn(1885, 785, 90, 90)) { ElementRemove("InputSearch"); CommonSetScreen("Character", "FriendList"); FriendListReturn = "ChatSearch"; }
-	if (MouseIn(1885, 885, 90, 90)) ChatSearchExit();
+		
+		// Display ignored rooms that are no longer present
+		var IgnoreOffset = ChatSearchResultOffset - ChatSearchResult.length + 1;
+		if (IgnoreOffset < 0) IgnoreOffset = 0;
+		for (let C = IgnoreOffset; C < ChatSearchIgnoredRooms.length && ShownRooms < ChatSearchRoomsPerPage; C++) {
+			var isIgnored = !ChatSearchResult.map(Room => Room.Name.toUpperCase()).includes(ChatSearchIgnoredRooms[C]);
+			if (isIgnored) {
+				var Hover = (MouseX >= X) && (MouseX <= X + 630) && (MouseY >= Y) && (MouseY <= Y + 85) && !CommonIsMobile;
+				
+				// Draw the room rectangle
+				DrawRect(X, Y, 630, 85, Hover ? "red" : "pink");
+				DrawTextFit(ChatSearchIgnoredRooms[C], X + 315, Y + 25, 620, "black");
+
+				// Moves the next window position
+				X = X + 660;
+				if (X > 1500) {
+					X = 25;
+					Y = Y + 109;
+				}
+				ShownRooms++;
+			}
+		}
+	} else DrawText(TextGet("NoChatRoomFound"), 1000, 450, "White", "Gray");
 }
 
 /**
- * Handles the key presses while in the chat search screen. When the user presses enter, we lauch the search query.
- * @returns {void} - Nothing
- */
-function ChatSearchKeyDown() {
-	if (KeyPress == 13) ChatSearchQuery();
-}
-
-/**
- * Handles exiting from the chat search screen, removes the input.
- * @returns {void} - Nothing
- */
-function ChatSearchExit() {
-	ChatSearchPreviousActivePose = Player.ActivePose;
-	ElementRemove("InputSearch");
-	CommonSetScreen("Room", ChatSearchLeaveRoom);
-}
-
-/**
- * Handles the clicks related to the chatroom list
+ * Handles the clicks related to the chatroom list when in normal mode
  * @returns {void} - Nothing
  */
 function ChatSearchJoin() {
 	
-	// Scans up to 24 results
+	// Scans results
 	var X = 25;
 	var Y = 25;
 	for (let C = ChatSearchResultOffset; C < ChatSearchResult.length && C < (ChatSearchResultOffset + ChatSearchRoomsPerPage); C++) {
@@ -179,6 +236,63 @@ function ChatSearchJoin() {
 	}
 }
 
+/** 
+ * Handles the clicks related to the chatroom list when in permission mode
+ * @returns {void} - Nothing
+ */
+function ChatSearchClickPermission() { 
+	// Scans results + hidden rooms
+	var X = 25;
+	var Y = 25;
+	var ShownRooms = 0;
+	for (let C = ChatSearchResultOffset; C < ChatSearchResult.length && ShownRooms < ChatSearchRoomsPerPage; C++) {
+
+		// If the player clicked on an existing room
+		if ((MouseX >= X) && (MouseX <= X + 630) && (MouseY >= Y) && (MouseY <= Y + 85)) {
+			var RoomName = ChatSearchResult[C].Name.toUpperCase();
+			var Idx = ChatSearchIgnoredRooms.indexOf(RoomName);
+			if (Idx != -1)
+				ChatSearchIgnoredRooms.splice(Idx, 1);
+			else
+				ChatSearchIgnoredRooms.push(RoomName);
+		}
+
+		// Moves the next window position
+		X = X + 660;
+		if (X > 1500) {
+			X = 25;
+			Y = Y + 109;
+		}
+		ShownRooms++;
+	}
+	
+	// Clicks for the extra hidden rooms
+	var IgnoreOffset = ChatSearchResultOffset - ChatSearchResult.length + 1;
+	if (IgnoreOffset < 0) IgnoreOffset = 0;
+	for (let C = IgnoreOffset; C < ChatSearchIgnoredRooms.length && ShownRooms < ChatSearchRoomsPerPage; C++) {
+		var isIgnored = !ChatSearchResult.map(Room => Room.Name.toUpperCase()).includes(ChatSearchIgnoredRooms[C]);
+		if (isIgnored) {
+			// If the click is valid
+			if ((MouseX >= X) && (MouseX <= X + 630) && (MouseY >= Y) && (MouseY <= Y + 85)) {
+				var RoomName = ChatSearchIgnoredRooms[C];
+				var Idx = ChatSearchIgnoredRooms.indexOf(RoomName);
+				if (Idx != -1)
+					ChatSearchIgnoredRooms.splice(Idx, 1);
+				else
+					ChatSearchIgnoredRooms.push(RoomName);
+			}
+
+			// Moves the next window position
+			X = X + 660;
+			if (X > 1500) {
+				X = 25;
+				Y = Y + 109;
+			}
+			ShownRooms++;
+		}
+	}
+}
+ 
 /**
  * Handles the reception of the server response when joining a room or when getting banned/kicked
  * @param {string} data - Response from the server
